7150e001b105916e5bd6c1ba943f38c9b6ac6c8d
AleksandrGHub <bulanov.shura@gmail.com>
2024-05-29 21:53:51 +0300

    Добавьте файлы проекта.

---
+++ Output control/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Output_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool hasStop = false;
+            string userInput;
+            string exitWord = "exit";
+
+            while (hasStop == false)
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput == exitWord)
+                {
+                    hasStop = true;
+                    Console.WriteLine("Программа остановлена!");
+                    return;
+                }
+            }
+        }
+    }
+}
